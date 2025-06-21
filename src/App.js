2d2c3c44455c19905f954b801b1464c34f6ab522
Alvaro Denis <denisacostaq@gmail.com>
2025-06-21 12:42:51 +0300

    feat: add router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import './styles.css'
 import Header from './components/header';
 import Footer from './components/footer';
 import MoviesGrid from './components/movies-grid';
+import WatchList from './components/watch-list';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+        <Router>
+          <nav>
+            <ul>
+              <Link to="/">Home</Link>
+              <Link to="/watchlist">Watch List</Link>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/' element={<MoviesGrid/>}></Route>
+            <Route path='/watchlist' element={<WatchList/>}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer></Footer>
     </div>

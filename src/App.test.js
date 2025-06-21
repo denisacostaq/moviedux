e56ef6fae494412108c477278967fe2f2d22ddaf
Alvaro Denis <denisacostaq@gmail.com>
2025-06-21 14:59:54 +0300

    test: App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders navigation links for Home and Watch List', () => {
+  // Render App directly, since it has its own Router
+  render(<App />); 
+  
+  const homeLink = screen.getByRole('link', { name: /home/i });
+  const watchListLink = screen.getByRole('link', { name: /watch list/i });
+
+  expect(homeLink).toBeInTheDocument();
+  expect(watchListLink).toBeInTheDocument();
 });

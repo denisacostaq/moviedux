0d629a070e618bc5ea67a1a562de09be00eb42bf
Alvaro Denis <denisacostaq@gmail.com>
2025-06-21 13:35:21 +0300

    feat: implement WatchList template

--- src/components/watch-list.js
+++ src/components/watch-list.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import '../styles.css'
+import MovieCard from './movie-card';
 
 export default function WatchList({movies, watchList, toggleWatchList}) {
     return (
         <div>
             <h1 className='title'>Your Watchlist</h1>
+            <div className='watchlist'>
+                {
+                    watchList.map(id => {
+                        const movie = movies.find(movie => movie.id === id)
+                        return (
+                            <MovieCard
+                              movie={movie}
+                              toggleWatchList={toggleWatchList}
+                              isWatchListed={true}>
+                            </MovieCard>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }

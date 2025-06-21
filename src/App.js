65e7abb96ac6012876deab596d4e1003c854e37d
Alvaro Denis <denisacostaq@gmail.com>
2025-06-21 08:32:30 +0300

    feat: use MoviesGrid to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import './styles.css'
 import Header from './components/header';
 import Footer from './components/footer';
+import MoviesGrid from './components/movies-grid';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Header></Header>
+        <MoviesGrid></MoviesGrid>
       </div>
       <Footer></Footer>
     </div>

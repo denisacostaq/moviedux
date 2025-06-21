f35c98eabded601344756c788be74bee467f911d
Alvaro Denis <denisacostaq@gmail.com>
2025-06-21 11:42:52 +0300

    feat: dynamic rating css class depending on value

--- src/components/movie-card.js
+++ src/components/movie-card.js
@@ -6,13 +6,23 @@ export default function MovieCard({movie}) {
         e.target.src = 'images/default.jpg';
     }
 
+    const getRatingClass = (rating) => {
+        if (rating >= 8) {
+            return 'rating-good';
+        }
+        if (rating >= 5 && rating < 8) {
+            return 'rating-ok';
+        }
+        return 'rating-bad';
+    }
+
     return (
         <div className='movie-card'>
             <img src={`images/${movie.image}`} alt={movie.title} onError={handleError}/>
             <div className='movie-card-info'>
                 <h3 className='movie-card-title'>{movie.title}</h3>
                 <p className='movie-card-genere'>{movie.genere}</p>
-                <p className='movie-card-rating'>{movie.rating}</p>
+                <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>{movie.rating}</p>
             </div>
         </div>
     )
